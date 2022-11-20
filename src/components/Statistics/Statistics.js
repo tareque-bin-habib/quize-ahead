6580f68ef61f1e652ab45811a98b1c70ff411cb4
tareque-bin-habib <shad.tbh@gmail.com>
2022-11-20 21:33:58 +0600

    chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,39 @@
 import React from 'react';
-
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './Statistics.css'
 const Statistics = () => {
+    const data = [
+        {
+            name: 'React',
+            total: 8
+        },
+        {
+            name: 'Javascript',
+            total: 9
+        },
+        {
+            name: 'CSS',
+            total: 8
+        },
+        {
+            name: 'Git',
+            total: 11
+        },
+    ]
+
     return (
-        <div>
-            <h2>This is state</h2>
+        <div className='d-flex align-items-center justify-content-center mt-5'>
+            <div>
+                <h3 className='logo'>Total Quize in Every Topics</h3>
+            </div>
+            <div>
+                <LineChart width={600} height={400} data={data}>
+                    <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
+            </div>
         </div>
     );
 };
